83a18599b47b06b0be3fa82535a2b344322d1076
Sumaa Sayed <ssayed1@umbc.edu>
2022-10-03 10:56:31 -0400

    added banner inputs
    Signed-off-by: Sumaa Sayed <ssayed1@umbc.edu>

--- controls/SV-230227.rb
+++ controls/SV-230227.rb
@@ -14,32 +14,7 @@ with human users and are not required when such human interfaces do not exist.
 the following verbiage for operating systems that can accommodate banners of
 1300 characters:
 
-    \"You are accessing a U.S. Government (USG) Information System (IS) that is
-provided for USG-authorized use only.
-
-    By using this IS (which includes any device attached to this IS), you
-consent to the following conditions:
-
-    -The USG routinely intercepts and monitors communications on this IS for
-purposes including, but not limited to, penetration testing, COMSEC monitoring,
-network operations and defense, personnel misconduct (PM), law enforcement
-(LE), and counterintelligence (CI) investigations.
-
-    -At any time, the USG may inspect and seize data stored on this IS.
-
-    -Communications using, or data stored on, this IS are not private, are
-subject to routine monitoring, interception, and search, and may be disclosed
-or used for any USG-authorized purpose.
-
-    -This IS includes security measures (e.g., authentication and access
-controls) to protect USG interests--not for your personal benefit or privacy.
-
-    -Notwithstanding the above, using this IS does not constitute consent to
-PM, LE or CI investigative searching or monitoring of the content of privileged
-communications, or work product, related to personal representation or services
-by attorneys, psychotherapists, or clergy, and their assistants. Such
-communications and work product are private and confidential. See User
-Agreement for details.\"
+    \"#{input('banner_message_text_cli')}\"
 
 
   "
@@ -55,32 +30,7 @@ the following command:
 
     If the banner is set correctly it will return the following text:
 
-    “You are accessing a U.S. Government (USG) Information System (IS) that is
-provided for USG-authorized use only.
-
-    By using this IS (which includes any device attached to this IS), you
-consent to the following conditions:
-
-    -The USG routinely intercepts and monitors communications on this IS for
-purposes including, but not limited to, penetration testing, COMSEC monitoring,
-network operations and defense, personnel misconduct (PM), law enforcement
-(LE), and counterintelligence (CI) investigations.
-
-    -At any time, the USG may inspect and seize data stored on this IS.
-
-    -Communications using, or data stored on, this IS are not private, are
-subject to routine monitoring, interception, and search, and may be disclosed
-or used for any USG-authorized purpose.
-
-    -This IS includes security measures (e.g., authentication and access
-controls) to protect USG interests--not for your personal benefit or privacy.
-
-    -Notwithstanding the above, using this IS does not constitute consent to
-PM, LE or CI investigative searching or monitoring of the content of privileged
-communications, or work product, related to personal representation or services
-by attorneys, psychotherapists, or clergy, and their assistants. Such
-communications and work product are private and confidential. See User
-Agreement for details.”
+    “#{input('banner_message_text_cli')}”
 
     If the banner text does not match the Standard Mandatory #{input('org_name')[:acronym]} Notice and
 Consent Banner exactly, this is a finding.
@@ -92,32 +42,7 @@ Banner before granting access to the system via command line logon.
     Edit the \"/etc/issue\" file to replace the default text with the Standard
 Mandatory #{input('org_name')[:acronym]} Notice and Consent Banner. The #{input('org_name')[:acronym]}-required text is:
 
-    \"You are accessing a U.S. Government (USG) Information System (IS) that is
-provided for USG-authorized use only.
-
-    By using this IS (which includes any device attached to this IS), you
-consent to the following conditions:
-
-    -The USG routinely intercepts and monitors communications on this IS for
-purposes including, but not limited to, penetration testing, COMSEC monitoring,
-network operations and defense, personnel misconduct (PM), law enforcement
-(LE), and counterintelligence (CI) investigations.
-
-    -At any time, the USG may inspect and seize data stored on this IS.
-
-    -Communications using, or data stored on, this IS are not private, are
-subject to routine monitoring, interception, and search, and may be disclosed
-or used for any USG-authorized purpose.
-
-    -This IS includes security measures (e.g., authentication and access
-controls) to protect USG interests -- not for your personal benefit or privacy.
-
-    -Notwithstanding the above, using this IS does not constitute consent to
-PM, LE or CI investigative searching or monitoring of the content of privileged
-communications, or work product, related to personal representation or services
-by attorneys, psychotherapists, or clergy, and their assistants. Such
-communications and work product are private and confidential. See User
-Agreement for details.\"
+    \"#{input('banner_message_text_cli')}\"
   "
   impact 0.5
   tag severity: 'medium'
