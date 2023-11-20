ec956e2c0a5fcbb8accbcd7e3d4c5c0ac4719d23
jmetzger <Jon.metzger@me.com>
2023-11-20 10:03:12 -0500

    sssd pam update

--- controls/SV-230372.rb
+++ controls/SV-230372.rb
@@ -83,20 +83,24 @@ restart the \"sssd\" service, run the following command:
   tag cci: ['CCI-000765']
   tag nist: ['IA-2 (1)']
 
-  if file(input('sssd_conf_path')).exist?
-    if virtualization.system.eql?('docker')
-      impact 0.0
-      describe 'Control not applicable within a container' do
-        skip 'Control not applicable within a container'
-      end
-    else
-      describe parse_config_file(input('sssd_conf_path')) do
-        its('pam') { should include('pam_cert_auth' => 'True') }
-      end
-    end
-  else
+  if !file(input('sssd_conf_path')).exist?
     describe "The sssd.conf file was not found at: #{input('sssd_conf_path')}" do
       skip "The sssd.conf file was not found at: #{input('sssd_conf_path')}"
+    end
+  elsif virtualization.system.eql?('docker')
+    impact 0.0
+    describe 'Control not applicable within a container' do
+      skip 'Control not applicable within a container'
+    end
+  else
+    describe parse_config_file(input('sssd_conf_path')) do
+      its('pam') { should include('pam_cert_auth' => 'True') }
+    end
+    describe pam('/etc/pam.d/system-auth') do
+      its('lines') { should match_pam_rule('auth   [success=done authinfo_unavail=ignore ignore=ignore default=die]   pam_sss.so try_cert_auth') }
+    end
+    describe pam('/etc/pam.d/smartcard-auth') do
+      its('lines') { should match_pam_rule('auth   sufficient   pam_sss.so try_cert_auth') }
     end
   end
 end
