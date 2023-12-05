89edf92825c045f66f9d70cd784d7b71b592f89b
Emily Rodriguez <ecrodriguez@mitre.org>
2023-12-05 14:48:03 -0600

    update TIPC to tipc according to updated guidance
    Signed-off-by: Emily Rodriguez <ecrodriguez@mitre.org>

--- controls/SV-230497.rb
+++ controls/SV-230497.rb
@@ -53,7 +53,7 @@ Reboot the system for the settings to take effect.'
       skip 'Control not applicable within a container'
     end
   else
-    describe kernel_module('TIPC') do
+    describe kernel_module('tipc') do
       it { should be_disabled }
       it { should be_blacklisted }
     end
