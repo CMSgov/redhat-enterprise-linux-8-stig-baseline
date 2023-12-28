e7d67cd316ce7a728c8d683f2e8415e2bd7a8e66
Jonathan Metzger <9845417+jrmetzger@users.noreply.github.com>
2023-12-28 10:59:04 -0500

    aide check update

--- controls/SV-251710.rb
+++ controls/SV-251710.rb
@@ -89,24 +89,24 @@ control 'SV-251710' do
   tag cci: ['CCI-002696']
   tag nist: ['SI-6 a']
 
-  file_integrity_tool_package = input('file_integrity_tool_package')
-  file_integrity_tool_service = input('file_integrity_tool_service')
+  file_integrity_tool = input('file_integrity_tool')
+  file_integrity_service = input('file_integrity_service')
 
   only_if('Control not applicable within a container', impact: 0.0) do
     !virtualization.system.eql?('docker')
   end
 
-  if file_integrity_tool_package == 'aide'
+  if file_integrity_tool == 'aide'
     describe command('/usr/sbin/aide --check') do
       its('exit_status') { should cmp 0 }
       its('stdout') { should_not include "Couldn't open file"}
     end
   end
 
-  describe package(file_integrity_tool_package) do
+  describe package(file_integrity_tool) do
     it { should be_installed }
   end
-  describe service(file_integrity_tool_service) do
+  describe service(file_integrity_service) do
     it { should be_enabled }
     it { should be_running }
   end
