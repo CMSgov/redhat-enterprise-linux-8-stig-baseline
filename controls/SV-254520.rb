3f2e77ad3cf4d516c6d3535a2f1a85ae4a84c3bc
wdower <57142072+wdower@users.noreply.github.com>
2023-12-28 17:37:10 +0000

    adding wip for 254520
    Signed-off-by: wdower <57142072+wdower@users.noreply.github.com>

--- controls/SV-254520.rb
+++ controls/SV-254520.rb
@@ -67,4 +67,33 @@ This must be documented with the information system security officer (ISSO) as a
   tag 'documentable'
   tag cci: ['CCI-002265']
   tag nist: ['AC-16 b']
+
+  se_login = command('semanage login -ln').stdout.lines.map(&:strip)
+
+  se_login.each_with_object({}) do |line, users|
+     login_name, selinux_user = line.split[0..1]  
+     users[login_name] = selinux_user  
+  end
+
+  misconfigured_admins = users.select{ |login_name, selinux_user| 
+     input('administrators').include?(login_name) &&
+     !input('allowed_admin_selinux_roles').include?(selinux_user)
+  }
+
+  misconfigured_non_admins = users.select{ |login_name, selinux_user| 
+     !input('administrators').include?(login_name) &&
+     !input('allowed_non_admin_selinux_roles').include?(selinux_user)
+  }
+
+  describe "All administrators" do
+     it "must be mapped to the an appropriate role (allowed admin roles: #{input(allowed_admin_selinux_roles).join(', ')})" do
+          expect(misconfigured_admins).to be_empty, "Misconfigured admins:\n\t- #{misconfigured_admins.join("\n\t- ")}"
+     end
+  end
+
+  describe "All non-administrator users" do
+     it "must be mapped to the an appropriate role (allowed non-admin user roles: #{input(allowed_non_admin_selinux_roles).join(', ')})" do
+          expect(misconfigured_admins).to be_empty, "Misconfigured non-admin users:\n\t- #{misconfigured_non_admins.join("\n\t- ")}"
+     end
+  end
 end
